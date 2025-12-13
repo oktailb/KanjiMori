e99ddf9d4a01455814a52717f4b4f05a3da5ec7d
LECOQ Vincent <110967411+oktailb@users.noreply.github.com>
2025-12-13 14:43:40 +0900

    Initial commit Reproduce basic Kanji Tree interface, will customize later when functional

---
+++ app/src/main/java/org/oktail/kanjimori/ui/reading/ReadingFragment.kt
@@ -0,0 +1,65 @@
+package org.oktail.kanjimori.ui.reading
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.oktail.kanjimori.databinding.FragmentReadingBinding
+
+class ReadingFragment : Fragment() {
+
+    private var _binding: FragmentReadingBinding? = null
+    private val binding get() = _binding!!
+
+    // Percentage variables
+    private var n5Percentage = 0.0
+    private var n4Percentage = 0.0
+    private var n3Percentage = 0.0
+    private var n2Percentage = 0.0
+    private var n1Percentage = 0.0
+    private var words1000Percentage = 0.0
+    private var words2000Percentage = 0.0
+    private var words3000Percentage = 0.0
+    private var words4000Percentage = 0.0
+    private var words5000Percentage = 0.0
+    private var words6000Percentage = 0.0
+    private var words7000Percentage = 0.0
+    private var words8000Percentage = 0.0
+    private var userListPercentage = 0.0
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentReadingBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        updateButtonText()
+
+        return root
+    }
+
+    private fun updateButtonText() {
+        binding.buttonReadingN5.text = "N5\n$n5Percentage%"
+        binding.buttonReadingN4.text = "N4\n$n4Percentage%"
+        binding.buttonReadingN3.text = "N3\n$n3Percentage%"
+        binding.buttonReadingN2.text = "N2\n$n2Percentage%"
+        binding.buttonReadingN1.text = "N1\n$n1Percentage%"
+        binding.buttonWords1000.text = "1000\n$words1000Percentage%"
+        binding.buttonWords2000.text = "2000\n$words2000Percentage%"
+        binding.buttonWords3000.text = "3000\n$words3000Percentage%"
+        binding.buttonWords4000.text = "4000\n$words4000Percentage%"
+        binding.buttonWords5000.text = "5000\n$words5000Percentage%"
+        binding.buttonWords6000.text = "6000\n$words6000Percentage%"
+        binding.buttonWords7000.text = "7000\n$words7000Percentage%"
+        binding.buttonWords8000.text = "8000\n$words8000Percentage%"
+        binding.buttonUserList.text = "Liste de l'utilisateur\n$userListPercentage%"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/org/oktail/kanjimori/ui/recognition/RecognitionFragment.kt
@@ -0,0 +1,71 @@
+package org.oktail.kanjimori.ui.recognition
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.oktail.kanjimori.databinding.FragmentRecognitionBinding
+
+class RecognitionFragment : Fragment() {
+
+    private var _binding: FragmentRecognitionBinding? = null
+    private val binding get() = _binding!!
+
+    // Variables to hold the percentage values
+    private var hiraganaPercentage = 0.0
+    private var katakanaPercentage = 0.0
+    private var n5Percentage = 0.0
+    private var n4Percentage = 0.0
+    private var n3Percentage = 0.0
+    private var n2Percentage = 0.0
+    private var n1Percentage = 0.0
+    private var class1Percentage = 0.0
+    private var class2Percentage = 0.0
+    private var class3Percentage = 0.0
+    private var class4Percentage = 0.0
+    private var class5Percentage = 0.0
+    private var class6Percentage = 0.0
+    private var test4Percentage = 0.0
+    private var test3Percentage = 0.0
+    private var testPre2Percentage = 0.0
+    private var test2Percentage = 0.0
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecognitionBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        updateButtonText()
+
+        return root
+    }
+
+    private fun updateButtonText() {
+        binding.buttonHiragana.text = "Hiragana\n$hiraganaPercentage%"
+        binding.buttonKatakana.text = "Katakana\n$katakanaPercentage%"
+        binding.buttonN5.text = "N5\n$n5Percentage%"
+        binding.buttonN4.text = "N4\n$n4Percentage%"
+        binding.buttonN3.text = "N3\n$n3Percentage%"
+        binding.buttonN2.text = "N2\n$n2Percentage%"
+        binding.buttonN1.text = "N1\n$n1Percentage%"
+        binding.buttonClass1.text = "Classe 1\n$class1Percentage%"
+        binding.buttonClass2.text = "Classe 2\n$class2Percentage%"
+        binding.buttonClass3.text = "Classe 3\n$class3Percentage%"
+        binding.buttonClass4.text = "Classe 4\n$class4Percentage%"
+        binding.buttonClass5.text = "Classe 5\n$class5Percentage%"
+        binding.buttonClass6.text = "Classe 6\n$class6Percentage%"
+        binding.buttonTest4.text = "Test 4\n$test4Percentage%"
+        binding.buttonTest3.text = "Test 3\n$test3Percentage%"
+        binding.buttonTestPre2.text = "Test Pre-2\n$testPre2Percentage%"
+        binding.buttonTest2.text = "Test 2\n$test2Percentage%"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
