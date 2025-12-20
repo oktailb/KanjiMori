d00e9e87143a464a0cb9adaccb68b8cac2f6a3af
LECOQ Vincent <110967411+oktailb@users.noreply.github.com>
2025-12-20 10:40:12 +0900

    adjust night mode color for game recap

--- app/src/main/java/org/oktail/kanjimori/ui/gamerecap/GameRecapFragment.kt
+++ app/src/main/java/org/oktail/kanjimori/ui/gamerecap/GameRecapFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -168,10 +169,12 @@ class GameRecapFragment : Fragment() {
         val balance = score.successes - score.failures
         val percentage = (balance.toFloat() / 10.0f).coerceIn(-1.0f, 1.0f)
 
+        val baseColor = ContextCompat.getColor(requireContext(), R.color.recap_grid_base_color)
+
         return when {
-            percentage > 0 -> lerpColor(Color.WHITE, Color.GREEN, percentage)
-            percentage < 0 -> lerpColor(Color.WHITE, Color.RED, -percentage)
-            else -> Color.WHITE
+            percentage > 0 -> lerpColor(baseColor, Color.GREEN, percentage)
+            percentage < 0 -> lerpColor(baseColor, Color.RED, -percentage)
+            else -> baseColor
         }
     }
 
